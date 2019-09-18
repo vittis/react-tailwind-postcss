ac5c85ace962c380605fc24a68d6d7595208f198
unknown <vitbolb1@gmail.com>
2019-09-17 22:37:18 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto px-3">
+        <h1 className="mt-3">Tailwind</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
